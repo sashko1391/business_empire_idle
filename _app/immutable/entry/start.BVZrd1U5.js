91ea93dab25ed8a25c7da1fb8f5ed7005982c4a0
sashko1391 <sashko1391@users.noreply.github.com>
2026-03-17 08:28:58 +0000

    deploy: 1696f771af0ef736e0dac59b78c391fea1753883

--- _app/immutable/entry/start.BVZrd1U5.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BwG8illF.js";import{w as t}from"../chunks/BRepH1pM.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.VwTG_cTt.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BFflcuHI.js";import{w as t}from"../chunks/pJjZYbTm.js";export{t as load_css,r as start};
